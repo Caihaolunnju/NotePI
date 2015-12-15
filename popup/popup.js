9dd32ad5a1aef9a0b02ac4863b17385e39023b81
xuqiangm <xuqiangm@gmail.com>
2015-12-15 15:16:11 +0800

    Revert "画刷、橡皮、保存功能"
    This reverts commit 2a18fc7e58ef073f7b505ffb0ea7f43b355accd1.

--- popup/popup.js
+++ popup/popup.js
@@ -1,44 +1,10 @@
 require.config({
     paths: {
         'jquery': '../vendor/jquery-2.1.4.min',
-        'cloudTest': 'cloudTest',
-        'notecloud': '../common/notecloud'
+        'cloudTest': 'cloudTest'
 　　}
 });
 
-requirejs(['jquery','cloudTest','notecloud'], function($,ct,notecloud){
-   //  ct.setup();
-   /*
-    * “画笔”和“橡皮”的功能
-    */
-    $('#brush').click(function(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "brush"});
-        });
-    });
-
-    $('#eraser').click(function(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "eraser"});
-        });
-    });
-
-    $('#save').click(function(){
-        chrome.tabs.query({active: true, currentWindow: true},function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "save"},function(response){
-            	var url=response.url;
-                var pathArray=response.pathArray;
-                console.debug(url);
-                console.debug(pathArray);
-                notecloud.page(url, function(page){
-                	page.shapes = pathArray;
-	                notecloud.sync(page,function(){
-	                	notecloud.page(url, function(page){
-	                        console.debug('同步后对象:%s', JSON.stringify(page));
-	                    });
-	                });
-                });
-            });
-         });
-    });
+requirejs(['cloudTest'], function(ct){
+    ct.setup();
 });
