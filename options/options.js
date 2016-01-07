7f3a3838b49b4e6db3c3b8e4bc3adee619fbe7b3
shelimingming <shelimingming@qq.com>
2016-01-07 19:04:04 +0800

    options.js

--- options/options.js
+++ options/options.js
@@ -16,8 +16,6 @@ chrome.runtime.sendMessage({cmd:"getColor",content:"green"}, function(response){
 
 });
 
-
-
 //点击保存按钮
 document.getElementById('save').onclick = function(){
 	//保存画笔颜色
@@ -37,8 +35,11 @@ document.getElementById('save').onclick = function(){
 		//document.write(response);
 	});
 	//给contentpage发消息
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {cmd:"setColor",content:color});
+	chrome.tabs.query({}, function(tabs) {
+		for (i in tabs) {
+			console.debug("aa");
+			chrome.tabs.sendMessage(tabs[i].id, {cmd:"setColor",content:color});
+		}
     });
     alert('保存成功:' + "颜色：" + color);
 }
